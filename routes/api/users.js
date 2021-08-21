c216926b7db852013e9870f229a910d15a0d019a
Kyle <kdleonard@icloud.com>
2021-08-21 14:17:33 -0500

    User API mostly set up

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 
-// @route GET api/users
-// @desc Test route
+const User = require("../../models/User");
+
+// @route Post api/users
+// @desc Register User
 // @access Public
 
-router.get("/", (req, res) => res.send("Users Route"));
+router.post(
+  "/",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please Include a Valid Email").isEmail(),
+    body(
+      "password",
+      "Please enter a password with 6 or more charaters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already Exists" }] });
+      }
+      //Get users gravitar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //Return jsonwebtoken
+      res.send("User Registerd");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+
+    res.send("Users route");
+  }
+);
 
 module.exports = router;
